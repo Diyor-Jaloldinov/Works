765a9e369579b31accdeb439a404f5013a3d3527
Diyor-Jaloldinov <diyor.jaloldinov@gmail.com>
2021-09-11 18:53:25 +0200

    updated the code made more faster

--- src/test/java/homewroks/check_box/checkBox.java
+++ src/test/java/homewroks/check_box/checkBox.java
@@ -4,18 +4,26 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import utilities.BrowserFactory;
 
 public class checkBox {
 
 
+    WebDriver driver;
+
+    @BeforeTest
+    public void setUp(){
+        driver = BrowserFactory.getDriver("edge");
+
+        driver.manage().window().maximize();
+    }
 
     @Test
     public void testCase1()   {
-        WebDriver driver = BrowserFactory.getDriver("edge");
 
-        driver.manage().window().maximize();
 
         driver.get("https://www.seleniumeasy.com/test/basic-checkbox-demo.html");
 
@@ -26,31 +34,36 @@ public class checkBox {
         Assert.assertTrue(driver.findElement(By.id("isAgeSelected")).isSelected());
         Assert.assertTrue(driver.findElement(By.id("txtAge")).isDisplayed());
 
-        driver.quit();
+
 
     }
 
     @Test
     public void testCase2(){
-        WebDriver driver = BrowserFactory.getDriver("edge");
 
-        driver.manage().window().maximize();
 
         driver.get("https://www.seleniumeasy.com/test/basic-checkbox-demo.html");
 
         WebElement button = driver.findElement(By.id("check1"));
         Assert.assertEquals(button.getAttribute("value"),"Check All");
         button.click();
-        Assert.assertTrue(driver.findElement(By.xpath("//label[text()='Option 1']/input")).isSelected());
-        Assert.assertTrue(driver.findElement(By.xpath("//label[text()='Option 2']/input")).isSelected());
-        Assert.assertTrue(driver.findElement(By.xpath("//label[text()='Option 3']/input")).isSelected());
-        Assert.assertTrue(driver.findElement(By.xpath("//label[text()='Option 4']/input")).isSelected());
+
+        for (int i =1;i<=4;i++){
+            Assert.assertTrue(driver.findElement(By.xpath("//label[text()='Option "+i+"']/input")).isSelected());
+        }
+
+
 
         Assert.assertEquals(button.getAttribute("value"),"Uncheck All");
 
+
+
+
+    }
+
+    @AfterTest
+    public void tearDown(){
         driver.quit();
-
-
     }
 
 
